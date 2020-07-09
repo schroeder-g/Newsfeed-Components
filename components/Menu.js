cfa406717474a9a33caa575239f808b1ed010295
Alex <schroedergoncalves@gmail.com>
2020-07-08 21:42:52 -0400

    completed nav bar dynamic creation, added new article

--- components/Menu.js
+++ components/Menu.js
@@ -10,24 +10,34 @@ let menuItems = [
 ];
 
 /* 
-  Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
-
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
-
-  The 'menuMaker' takes an array as its only argument.
-
-  Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
-  Step 5: return your div with a 'menu' class.
-
-  Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
+Write a component called 'menuMaker' to create a menu
 */
+function menuMaker (navArray){
+  const navBar = document.createElement("div")
+  navBar.classList.add("menu")
+  const navList = document.createElement("ul")
+  const header = document.querySelector(".header")
+
+  //iterate over the array creating a list item <li> element for each item in the array. 
+  navArray.forEach((link) => {
+    const htmlLink = document.createElement("li")
+    htmlLink.innerText = link
+    htmlLink.id = link.replace(" ", "-").replace(/[',!?~@#$%^&*]/, "").toLowerCase() 
+    htmlLink.className = "navLink"
+    navList.appendChild(htmlLink)
+  })
+  
+  navBar.appendChild(navList)
+
+  // select the menu button (the element with a class of 'menu-button') currently on the DOM.
+  const menuBtn = document.querySelector(".menu-button")
+  // add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  menuBtn.addEventListener('click', () => {
+    navBar.classList.toggle("menu--open")
+  } )
+
+  header.appendChild(navBar)
+
+}
+
+menuMaker(menuItems)
